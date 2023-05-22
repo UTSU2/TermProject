f93b0832faac60e2b3e4cd5889c0d73365936fc2
UTSU2 <ejy6125@gmail.com>
2023-05-22 10:43:31 +0900

    b version

--- DodgeLaser.c
+++ DodgeLaser.c
@@ -24,7 +24,7 @@
 
 //조작 및 캐릭터 관련
 #define STAR '*'
-#define LASER '#'
+#define LASER '#' //*로 대체할 예정
 #define STAR1 "★" // player1 표시
 #define BLANK "  " // ' ' 로하면 흔적이 지워진다 
 
@@ -43,12 +43,28 @@
 
 //전역변수
 int Delay = 50; // 100 msec delay, 이 값을 줄이면 속도가 빨라진다.
-//int keep_moving = 1; // 1:계속이동, 0:한칸씩이동.
+int keep_moving = 1; // 1:계속이동, 0:한칸씩이동.
 //int time_out = 30; // 제한시간
-int score[2] = { 0 };
+int xLaser[WIDTH][HEIGHT] = {0};
+int yLaser[WIDTH][HEIGHT] = { 0 };
+int Laser_count = 0;
+int laserinterval = 4;
+int score[100] = { 0 };
+int user_score[1] = { 0 };
 int item[WIDTH][HEIGHT] = { 0 }; // 1이면 Gold 있다는 뜻
-int iteminterval = 7; // GOLD 표시 간격
-//int called[2];
+int iteminterval = 30; // GOLD 표시 간격
+int called[2];
+int frame_count = 0;
+int Laser_frame_sync = 5;
+int p_frame_sync = 10;
+int p_frame_sync_start = 0;
+
+//레이저 구조체
+struct sLaser {
+	int set;
+	int x;
+	int y;
+};
 
 void gotoxy(int x, int y) //내가 원하는 위치로 커서 이동
 {
@@ -125,38 +141,75 @@ void draw_box2(int x1, int y1, int x2, int y2)
 	gotoxy(x2, y1); printf("┐");
 	gotoxy(x2, y2); printf("┘");
 }
-void draw_box2_fill(int x1, int y1, int x2, int y2, int color)
+
+void player1(unsigned char ch)
 {
-	int x, y;
+	static int oldx = 10, oldy = 10, newx = 10, newy = 10;
+	int move_flag = 0;
+	static unsigned char last_ch = 0;
 
-	textcolor(WHITE, BLACK);
-	for (x = x1; x <= x2; x += 2) { // 한글은 2칸씩 차지한다.
-		gotoxy(x, y1);
-		printf("%s", "─"); // ㅂ 누르고 한자키 누르고 선택
-		gotoxy(x, y2);
-		printf("%s", "─");
+	if (called[0] == 0) { // 처음 또는 Restart
+		oldx = 10, oldy = 10, newx = 10, newy = 10;
+		putstar(oldx, oldy, STAR1);
+		called[0] = 1;
+		last_ch = 0;
+		ch = 0;
 	}
-	for (y = y1; y <= y2; y++) {
-		gotoxy(x1, y);
-		printf("%s", "│");
-		gotoxy(x2, y);
-		printf("%s", "│");
-	}
-	gotoxy(x1, y1); printf("┌");
-	gotoxy(x1, y2); printf("└");
-	gotoxy(x2, y1); printf("┐");
-	gotoxy(x2, y2); printf("┘");
-	//getch();
-	for (y = y1 + 1; y < y2; y++) {
-		for (x = x1 + 2; x < x2; x++) {
-			textcolor(color, color);
-			gotoxy(x, y);
-			printf(" ");
+	// 같은 방향으로 key 가 들어오면 무시한다.
+	if (last_ch == ch && frame_count % p_frame_sync != 0)
+		return;
+	if (keep_moving && ch == 0)
+		ch = last_ch;
+	last_ch = ch;
+
+	switch (ch) {
+	case UP:
+		if (oldy > 1) // 0 은 Status Line
+			newy = oldy - 1;
+		else { // 벽에 부딛치면 방향을 반대로 이동
+			newy = oldy + 1;
+			last_ch = DOWN;
 		}
+		move_flag = 1;
+		break;
+	case DOWN:
+		if (oldy < HEIGHT - 3)
+			newy = oldy + 1;
+		else {
+			newy = oldy - 1;
+			last_ch = UP;
+		}
+		move_flag = 1;
+		break;
+	case LEFT:
+		if (oldx > 2)
+			newx = oldx - 2;
+		else {
+			newx = oldx + 2;
+			last_ch = RIGHT;
+		}
+		move_flag = 1;
+		break;
+	case RIGHT:
+		if (oldx < WIDTH - 4)
+			newx = oldx + 2;
+		else {
+			newx = oldx - 2;
+			last_ch = LEFT;
+		}
+		move_flag = 1;
+		break;
 	}
-
+	if (move_flag) {
+		erasestar(oldx, oldy); // 마지막 위치의 * 를 지우고
+		putstar(newx, newy, STAR1); // 새로운 위치에서 * 를 표시한다.
+		oldx = newx; // 마지막 위치를 기억한다.
+		oldy = newy;
+	}
 }
+
 void StartMenu() {
+	//초기화면
 	gotoxy(2, 1);
 	textcolor(YELLOW2, BLACK);
 	printf("■       ■■■■ ■■■■ ■■■■ ■■■■\n");
@@ -188,6 +241,23 @@ void StartMenu() {
 	printf("■■■■■");
 	gotoxy(63, 5);
 	printf("■  ■");
+	//초기설정
+	int x, y;
+	srand(time(NULL));
+	user_score[0] = 0;
+	for (x = 0; x < WIDTH; x++) {
+		for (y = 0; y < HEIGHT; y++) {
+			item[x][y] == 0;
+		}
+	}
+	Laser_count = 0;
+	iteminterval = 30;
+	score[0] = 0;
+	called[0] = called[1] = 0;
+	frame_count = 0;
+	p_frame_sync = 10;
+	p_frame_sync_start = 0;
+	Laser_frame_sync = 5;
 
 	while (1) {
 		int c1, c2;
@@ -200,6 +270,10 @@ void StartMenu() {
 		printf("- 시작하려면 아무 키나 입력하십시오... -");
 		Sleep(300);
 		if (_kbhit()) {
+			system("mode con cols=88 lines=25");
+			cls(BLACK, WHITE);
+			textcolor(WHITE, BLACK);
+			draw_box2(0, 0, 78, 22);
 			break;
 		}
 	}
@@ -260,9 +334,103 @@ void shootLaser(int Lx, int Ly, int set) {
 	//Ly = rand() % 20 + 1; //레이저 2줄 -> 최대값 1 줄임
 }
 
+void Laser_start() {
+	int x, y, set;
+	set = rand() % 10;
+	if (set < 7) {
+		x = rand() % (WIDTH - 2) + 2;
+		textcolor(WHITE, WHITE);
+		gotoxy(x, 1);
+		printf("*");
+		xLaser[x][1] = 1;
+		Laser_count++;
+		textcolor(WHITE, BLACK);
+	}
+	else {
+		y = rand() % (HEIGHT - 4) + 1;
+		textcolor(WHITE, WHITE);
+		gotoxy(2, y);
+		printf("*");
+		gotoxy(2, y + 1);
+		printf("*");
+		yLaser[2][y] = 1;
+		Laser_count++;
+		textcolor(WHITE, BLACK);
+	}
+}
+
+void xLaser_shoot() {
+	int x, y;
+	
+	if (Laser_count == 0)
+		return;
+	for (x = 0; x < WIDTH; x++) {
+		for (y = 0; y < HEIGHT; y++) {
+			if (xLaser[x][y] && xLaser[x][y + 1] != 1) {
+				if (y > 5 && y < HEIGHT - 5) {
+					gotoxy(x, y - 4);
+					printf(BLANK);
+					xLaser[x][y - 4] = 0;
+					gotoxy(x, y + 1);
+					textcolor(WHITE, WHITE);
+					printf("*");
+					textcolor(WHITE, BLACK);
+					xLaser[x][y + 1] = 1;
+				}
+				else if (y > 5) {
+					gotoxy(x, y - 4);
+					printf(BLANK);
+					xLaser[x][y - 4] = 0;
+				}
+				else {
+					gotoxy(x, y + 1);
+					textcolor(WHITE, WHITE);
+					printf("*");
+					textcolor(WHITE, BLACK);
+					xLaser[x][y + 1] = 1;
+				}
+			}
+		}
+	}
+}
+
+void yLaser_shoot() {
+	int x, y;
+
+	if (Laser_count == 0)
+		return;
+	for (x = 0; x < WIDTH; x++) {
+		for (y = 0; y < HEIGHT; y++) {
+			if (yLaser[x][y] && yLaser[x+1][y] != 1) {
+				if (x > 5 && x < WIDTH - 5) {
+					gotoxy(x - 4, y);
+					printf(BLANK);
+					yLaser[x - 4][y] = 0;
+					gotoxy(x + 1, y);
+					textcolor(WHITE, WHITE);
+					printf("*");
+					textcolor(WHITE, BLACK);
+					yLaser[x + 1][y] = 1;
+				}
+				else if (x > 5) {
+					gotoxy(x - 4, y);
+					printf(BLANK);
+					yLaser[x - 4][y] = 0;
+				}
+				else {
+					gotoxy(x + 1, y);
+					textcolor(WHITE, WHITE);
+					printf("*");
+					textcolor(WHITE, BLACK);
+					yLaser[x + 1][y] = 1;
+				}
+			}
+		}
+	}
+}
+
 void show_time(int remain_time)
 {
-	// 완성할 것
 	gotoxy(31, 24);
 	textcolor(WHITE, YELLOW1);
 	printf("시간 : %02d", remain_time);
@@ -273,7 +441,6 @@ void main()
 {
 	unsigned char ch; // 특수키 0xe0 을 입력받으려면 unsigned char 로 선언해야 함
 	int oldx, oldy, newx, newy;
-	int keep_moving;
 
 	int run_time, start_time, remain_time, last_remain_time;
 	int laser_time = 0;
@@ -282,91 +449,61 @@ void main()
 	newx = oldx = 10;
 	newy = oldy = 10;
 
-	char buf[100];
+//START:
+	laser_time = 0;
+	lasercount = 0;
 	removeCursor(); // 커서를 안보이게 한다
-
 	system("mode con cols=80 lines=24");
 	cls(BLACK, WHITE);
 	draw_box2(0, 0, 78, 22);
 	StartMenu(); //시작화면
 
-	system("mode con cols=88 lines=25");
-	cls(BLACK, WHITE);
-	textcolor(WHITE, BLACK);
-	draw_box2(0, 0, 78, 22);
 	putstar(oldx, oldy, STAR);
 	ch = 0;
 	keep_moving = 0;
-	srand(time(NULL));
+	//srand(time(NULL));
 	start_time = time(NULL);
 	remain_time = 0;
 	show_time(remain_time);
 	while (1) {
 		run_time = time(NULL) - start_time;
-		if (run_time > laser_time && (run_time % 2 == 0)) {
-			sh = rand() % 10;
-			if (sh > 7) {
-				Lx = rand() % 76 + 2;
-				Ly = rand() % 20 + 1;
-			}
-			else {
-				Lx = rand() % 76 + 2;
-				Ly = rand() % 20 + 1;
-			}
-			senseLaser(Lx, Ly, sh);
+		if (run_time > laser_time && (run_time % laserinterval == 0)) {
+			Laser_start();
 			laser_time = run_time;
-			if (lasercount % 3 == 0) {
-				shootLaser(Lx, Ly, sh);
-			}
 		}
-		lasercount++;
+
 		show_time(run_time);
 		if (_kbhit() == 1) {
-			ch = _getch();
+			char c1;
+			c1 = _getch();
+			if (c1 == '\0')
+				continue;
+			else
+				ch = c1;
 			if (ch == ESC) {
-				break;
+				exit(0);
 			}
-			if (ch == SPECIAL1 || ch == SPECIAL2) {
+			if (ch == SPECIAL1) {
 				ch = _getch();
 				switch (ch) {
 				case UP:
 				case DOWN:
 				case LEFT:
 				case RIGHT:
-					keep_moving = 1;
+					player1(ch);
 					break;
 				default:
-					keep_moving = 0;
+					if (frame_count % p_frame_sync == 0)
+						player1(0);
 				}
 			}
 		}
-		if (keep_moving) {
-			switch (ch) {
-			case UP:
-				if (oldy > 1)
-					newy = oldy - 1;
-				break;
-			case DOWN:
-				if (oldy < HEIGHT - 3)
-					newy = oldy + 1;
-				break;
-			case LEFT:
-				if (oldx > 2)
-					newx = oldx - 2;
-				break;
-			case RIGHT:
-				if (oldx < WIDTH - 4)
-					newx = oldx + 2;
-				break;
-			}
-			erasestar(oldx, oldy);
-			putstar(newx, newy, STAR1);
-			oldx = newx; //마지막 위치 기억
-			oldy = newy;
-			keep_moving = 0; //한 번에 한칸씩만 이동할 예정
+		if (frame_count % Laser_frame_sync == 0) {
+			xLaser_shoot();
+			yLaser_shoot();
 		}
-		
 		Sleep(Delay);
+		frame_count++;
 	}
 
 	textcolor(BLACK, WHITE);
